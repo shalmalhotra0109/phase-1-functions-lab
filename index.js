9ada0c4b20deae348e517268c479a8036ca34932
Shal Malhotra <smalhotra0109@gmail.com>
2023-10-07 14:47:02 -0400

    functions practice

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const hqLocation = 42
+const blockToFeet = 264
+function distanceFromHqInBlocks(blockNumber) {
+  return Math.abs(blockNumber - hqLocation)
+}
+console.log(distanceFromHqInBlocks(43))
+
+
+
+function distanceFromHqInFeet(blockNumber) {
+  let numberOfBlocks = distanceFromHqInBlocks(blockNumber);
+  return numberOfBlocks * blockToFeet
+
+}
+
+
+
+function distanceTravelledInFeet(start, destination) {
+  let numberOfBlocks = Math.abs(destination - start)
+  return numberOfBlocks * blockToFeet
+
+}
+
+const freeRide = 400
+const longRide = 2000
+const maxRide = 2500
+function calculatesFarePrice(start, destination) {
+  let distanceInFeet = distanceTravelledInFeet(start, destination)
+  if (distanceInFeet <= freeRide)
+    return 0
+  else if (distanceInFeet > maxRide)
+    return "cannot travel that far"
+  else if (distanceInFeet >= longRide)
+    return 25
+  else
+    return (distanceInFeet - freeRide) * .02
+
+}
+
